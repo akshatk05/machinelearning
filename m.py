ff61e147fe931d486b8f1516138eaf71c7bf4f9c
akshatk05 <39399446+akshatk05@users.noreply.github.com>
2018-06-15 11:39:09 +0530

    Add files via upload

---
+++ m.py
@@ -0,0 +1,89 @@
+import math
+import sig
+import numpy as np
+
+def sig(x):
+ return(sig.sigmoid(x))
+def add(x,y):
+ return(x + y)
+def sub(x,y):
+ return(x - y)
+def mul(x,y):
+ return(x*y)
+def pow(x,y):
+ return(x ** y)
+def mod(x,y):
+ return(x % y)
+def rem_div(x,y):
+ return(x & y)
+def div(x,y):
+ return(x/y)
+def and_gate(x,y):
+ if x == 1 and y == 1:
+  return(True)
+ else:
+  return(False)
+def or_gate(x,y):
+ if x == 1 or y == 1:
+  return(True)
+ else:
+  return(False)
+def xor_gate():
+ if x == 1 or y == 1:
+  return(True)
+ elif x == 0 and y == 0:
+  return(True)
+ else:
+  return(false)
+def pi():
+ return(math.pi)
+def e():
+ return(math.e)
+def fact(x):
+ return(math.factorial(x))
+def abs(x):
+ return(abs(x))
+def help():
+ return(dir(m))
+def ln(x):
+ return(math.log(x))
+def log(x,y):
+ return(math.log(x)/math.log(y))
+def not_gate(x,y):
+ if x != 1 and y != 1:
+  return(True)
+ else:
+  return(False)
+def sin(x):
+ return(math.sin(x))
+def cos(x):
+ return(math.cos(x))
+def tan(x):
+ return(math.tan(x))
+def hypot(a,b):
+ math.hypot(x,y)
+def one_side(a,b):
+ return(a**2 - b)
+def deg(x):
+ return(math.deg(x))
+def gamma(x):
+ return(math.gamma(x))
+def sigma(x):
+	return((n**2 + n)/2)
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
